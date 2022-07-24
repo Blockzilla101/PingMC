df9871d3ba9c4a5d2154628e83a76dcd89245ea8
Blockzilla <48705529+Blockzilla101@users.noreply.github.com>
2022-07-24 12:35:46 +0500

    Update PingMC.ts

--- src/PingMC.ts
+++ src/PingMC.ts
@@ -14,20 +14,22 @@ export class PingMC {
         await conn.write(manager.createHandshakePacket());
         await conn.write(manager.createEmptyPacket());
 
-        const timeout = setTimeout(() => {
-            throw new Error('connection timed out');
-        }, 5 * 1000);
+        return new Promise(async (resolve, reject) => {
+            const timeout = setTimeout(() => {
+                reject(new Error('connection timed out'));
+            }, 5 * 1000);
 
-        conn.readable.pipeTo(decoder.writeable);
+            conn.readable.pipeTo(decoder.writeable);
 
-        await decoder.finish();
-        clearTimeout(timeout);
+            await decoder.finish();
+            clearTimeout(timeout);
 
-        const result = decoder.handShakePacket.result;
-        if (result.ping == null) {
-            result.ping = decoder.pingPacket.result;
-        }
+            const result = decoder.handShakePacket.result;
+            if (result.ping == null) {
+                result.ping = decoder.pingPacket.result;
+            }
 
-        return result;
+            resolve(result);            
+        })
     }
 }
